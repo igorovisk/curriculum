d31aeee043bfe4337174ba403d2b1e81ec15c674
igorovisk <igorovisk@gmail.com>
2021-09-15 15:42:17 -0300

    menu bug: not closing when clicking outside fix/container sections order change and color/background color changes

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -1,24 +1,57 @@
 "use strict"
    
 const ToggleMenu = () => {
+    
     const toggle = document.getElementById('nav-toggle')
     let navMenu = document.getElementById('nav-menu')    
-    const linkList = document.querySelectorAll('.nav-item')    
-       
-    if(toggle && navMenu){
+    const linkList = document.querySelectorAll('.nav-item')  
 
-        toggle.addEventListener('click', ()=>{ 
-            navMenu.classList.toggle('show-menu')                
-        })   
+    let menuDesativado = true
+             
+    if (menuDesativado){            
 
-        linkList.forEach(link => {               
-            link.addEventListener('click', () => {
-                navMenu.classList.toggle('show-menu')
-            })
-        })   
+        toggle.addEventListener('click', ()=>{                 
+            navMenu.classList.toggle('show-menu') 
+            menuDesativado = false             
+        })          
+
+        linkList.forEach(link => {    
+                    
+            link.addEventListener('click', () => {  
+                navMenu.classList.add('show-menu')
+                menuDesativado = false        
+            })              
+        })        
     }
-}
+
+    navMenu.classList.remove('show-menu')
+    menuDesativado = false  
+
+    if(!menuDesativado){
+        document.addEventListener('click', function(event) {
+
+            var isClickInside = toggle.contains(event.target);                    
+            if (!isClickInside && !menuDesativado) {
+                navMenu.classList.remove('show-menu')                
+                menuDesativado = true    
+            } 
+        })
+    }       
+} 
+    
+
+
+
+
+        
+       
+        
+      
+              
+
+
 
 ToggleMenu()
+
 
 export default ToggleMenu
